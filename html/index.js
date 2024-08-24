28b362c075b080151f860b0ffc075be9122d9046
ArthurSRods <arthur.srodrigues@hotmail.com>
2024-08-24 18:44:34 -0300

    revisão - básicos de HTML

---
+++ html/index.js
@@ -0,0 +1,11 @@
+console.log('esse trecho será exibido no console')
+
+alert('Olá mundo!')
+
+alert('Dá pra usar o atalho script:src no código para inserir um script')
+
+confirm('Janela de confirmação')
+
+alert('Se o usuário clicar em ok, retorna TRUE. Se clicar em cancelar, retorna FALSE')
+
+prompt('Funciona similar à função input do python')
